fe4567c477195938b4d30e2db042b52a57494b86
Michael Seiler <michaelseiler@Michaels-MBP.home>
2018-11-11 22:14:44 -0500

    youtube layout changes

--- src/YoutubeComponent.js
+++ src/YoutubeComponent.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import YouTube from 'react-youtube';
 
-
 class YoutubeComponent extends Component {
+  state = { showVideo:false}
+
+  constructor(props) {
+    super(props);
+        this.toggleVideo = this.toggleVideo.bind(this);
+
+  }
+
+  toggleVideo(){
+    //this.state.showVideo = !this.state.showVideo;
+    this.setState((state,props) => {
+      return {showVideo:!state.showVideo}
+    });
+    //this.state.showVideo = !this.state.showVideo;
+  }
 
   render() {
+    var postStyle = {
+      padding: 10,
+      margin:10,
+      backgroundColor:"#eff0f1",
+      color:"#333",
+      dipslay:"inline-block"
+    }
     var post = this.props.item.post;
+    var embed = <div></div>
+    if(this.state.showVideo){
+      embed = <div width="100%"><YouTube videoId={post.youtube_id} /></div>
+    }else{
+      embed = <img width="100%" src={post.image_url} />
+    }
     return ( 
-      <div>
-        <div>{post.organization_name}</div>
+      <div style={postStyle}>
+        <div onClick={this.toggleVideo}>{embed}</div>
+        <h4>{post.attachment_title}</h4>
         <div>
-         {post.username}
+        <span>@{post.username}</span> | 
+        <span>{post.plays_count} views </span> | <span>{post.created}</span>
         </div>
-        <YouTube videoId={post.youtube_id} />
       </div>
     )
   }
@@ -23,3 +51,4 @@ class YoutubeComponent extends Component {
 export default YoutubeComponent
 
 
+        //<YouTube videoId={post.youtube_id} />
