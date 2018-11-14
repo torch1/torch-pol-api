103428a0107d389c811bef9a41582d6c42f2b8d6
Michael Seiler <michaelseiler@Michaels-MBP.home>
2018-11-14 09:35:02 -0500

    Updates

--- precache-manifest.bac1ccea33131148a6028d7c0d9b00d4.js
+++ precache-manifest.bac1ccea33131148a6028d7c0d9b00d4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "5defc42d3e5c7f32c8ad",
+    "revision": "65fa0c531651ca3788c6",
     "url": "/torch-pol-api/static/css/main.e136a07a.chunk.css"
   },
   {
-    "revision": "5defc42d3e5c7f32c8ad",
-    "url": "/torch-pol-api/static/js/main.5defc42d.chunk.js"
+    "revision": "65fa0c531651ca3788c6",
+    "url": "/torch-pol-api/static/js/main.65fa0c53.chunk.js"
   },
   {
     "revision": "ff144f702848ed47c608",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/torch-pol-api/static/media/logo.5d5d9eef.svg"
   },
   {
-    "revision": "26bd830e554f3145b3e28cead76a8872",
+    "revision": "ee2b39374aff1b370a43872ee05f81f8",
     "url": "/torch-pol-api/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/torch-pol-api/precache-manifest.f672c455fd6de3fa4631406f48a4177e.js"
+  "/torch-pol-api/precache-manifest.bac1ccea33131148a6028d7c0d9b00d4.js"
 );
 
 workbox.clientsClaim();
